641f93f14b5513fee0da204e92071b2da78c4167
Your Name <you@example.com>
2022-10-14 11:31:48 -0500

    arreglado

--- model/conexion.php
+++ model/conexion.php
@@ -1,11 +1,11 @@
 <?php 
-$contrasena = "fortachon2121";
-$usuario = "cristhian";
+$contrasena = "Jg2JP9n3XpUdHJ";
+$usuario = "Tecsup";
 $nombre_bd = "crud";
 
 try {
 	$bd = new PDO (
-		'mysql:host=localhost;
+		'mysql:host=190.119.144.252;
 		dbname='.$nombre_bd,
 		$usuario,
 		$contrasena,
